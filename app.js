432987fdfbb857ceb88726ac389357e4920aa905
Rick Wu <rickwu316@gmail.com>
2021-02-04 04:17:03 +0800

    add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars') // 引入 handlebars
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const db = require('./models') // 引入資料庫
 const port = 3000
@@ -11,6 +12,8 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }))// Handlebars 註
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -22,6 +25,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
