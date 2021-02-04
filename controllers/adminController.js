10cb9867a481ebcba96e3bb54f4f98f2f0591fca
Rick Wu <rickwu316@gmail.com>
2021-02-04 14:32:32 +0800

    Add admin restaurant delete feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -60,7 +60,15 @@ const adminController = {
                     })
             })
     },
-
+    deleteRestaurant: (req, res) => {
+        return Restaurant.findByPk(req.params.id)
+            .then((restaurant) => {
+                restaurant.destroy()
+                    .then((restaurant) => {
+                        res.redirect('/admin/restaurants')
+                    })
+            })
+    }
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ module.exports = (app, passport) => {
     app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
     app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
     app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
+    app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
     app.get('/signup', userController.signUpPage)
     app.post('/signup', userController.signUp)
