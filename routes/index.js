0148e4bf157be3aec165565efe6bb200d684409b
Rick Wu <rickwu316@gmail.com>
2021-02-04 14:31:19 +0800

    Add admin restaurant put feature

--- routes/index.js
+++ routes/index.js
@@ -26,8 +26,10 @@ module.exports = (app, passport) => {
     // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
     app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
     app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+    app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
     app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
     app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+    app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
 
     app.get('/signup', userController.signUpPage)
     app.post('/signup', userController.signUp)
