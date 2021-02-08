61401d8e4d11d8e6ca47b7d033d430b187e63fa3
Rick Wu <rickwu316@gmail.com>
2021-02-09 02:46:57 +0800

    Show category name on admin/restaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,20 +1,28 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
     getRestaurants: (req, res) => {
-        return Restaurant.findAll({ raw: true }).then(restaurants => {
+        return Restaurant.findAll({
+            raw: true,
+            nest: true,
+            include: [Category]
+        }).then(restaurants => {
+            // console.log(restaurants) // 加入 console 觀察資料的變化
             return res.render('admin/restaurants', { restaurants: restaurants })
         })
     },
     getRestaurant: (req, res) => {
-        return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+        return Restaurant.findByPk(req.params.id, {
+            include: [Category]
+        }).then(restaurant => {
             return res.render('admin/restaurant', {
-                restaurant: restaurant
+                restaurant: restaurant.toJSON()
             })
         })
     },
