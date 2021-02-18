39a9b9d93d62602bf2a31ef9f3b6d597e9ab47f1
Rick Wu <rickwu316@gmail.com>
2021-02-18 15:53:46 +0800

    Add favorite/unfavorite feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,12 @@ const db = require('../models')
 const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
-const fs = require('fs')
+// const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const helpers = require('../_helpers')
-const restaurant = require('../models/restaurant')
+// const restaurant = require('../models/restaurant')
+const Favorite = db.Favorite
 
 const userController = {
     signUpPage: (req, res) => {
@@ -118,6 +119,29 @@ const userController = {
                 })
         }
 
+    },
+    addFavorite: (req, res) => {
+        return Favorite.create({
+            UserId: req.user.id,
+            RestaurantId: req.params.restaurantId
+        })
+            .then((restaurant) => {
+                return res.redirect('back')
+            })
+    },
+    removeFavorite: (req, res) => {
+        return Favorite.findOne({
+            where: {
+                UserId: req.user.id,
+                RestaurantId: req.params.restaurantId
+            }
+        })
+            .then((favorite) => {
+                favorite.destroy()
+                    .then((restaurant) => {
+                        return res.redirect('back')
+                    })
+            })
     }
 }
 
