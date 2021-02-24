eff08c638db3b18fa9427776d1a26f25e454f58b
Rick Wu <rickwu316@gmail.com>
2021-02-25 02:05:55 +0800

    refactor adminController.getCategories

--- services/adminService.js
+++ services/adminService.js
@@ -23,9 +23,29 @@ const adminService = {
         }).then(restaurant => {
             callback({ restaurant: restaurant.toJSON() })
         })
-    }
+    },
+    getCategories: (req, res, callback) => {
+        return Category.findAll({
+            raw: true,
+            nest: true
+        }).then(categories => {
+            if (req.params.id) {
+                Category.findByPk(req.params.id)
+                    .then((category) => {
+                        return callback({
+                            categories: categories,
+                            category: category.toJSON()
+                        })
+                    })
+            } else {
+                return callback({ categories: categories })
+            }
+        }).catch(error => console.error(error))
+    },
 
 }
+
+
 
 module.exports = adminService
 
