9825ccdbb65fa2d2ce068d9314e2a9cd1de149ba
Rick Wu <rickwu316@gmail.com>
2021-02-24 15:57:35 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,17 +5,23 @@ const Category = db.Category
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const adminService = require('../services/adminService.js')
 
 const adminController = {
     getRestaurants: (req, res) => {
-        return Restaurant.findAll({
-            raw: true,
-            nest: true,
-            include: [Category]
-        }).then(restaurants => {
-            // console.log(restaurants) // 加入 console 觀察資料的變化
-            return res.render('admin/restaurants', { restaurants: restaurants })
+        // return Restaurant.findAll({
+        //     raw: true,
+        //     nest: true,
+        //     include: [Category]
+        // }).then(restaurants => {
+        //     // console.log(restaurants) // 加入 console 觀察資料的變化
+        //     return res.render('admin/restaurants', { restaurants: restaurants })
+        // })
+
+        adminService.getRestaurants(req, res, (data) => {
+            return res.render('admin/restaurants', data)
         })
+
     },
     getRestaurant: (req, res) => {
         return Restaurant.findByPk(req.params.id, {
